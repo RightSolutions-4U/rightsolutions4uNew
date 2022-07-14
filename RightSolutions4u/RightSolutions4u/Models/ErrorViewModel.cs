b1337cb467126a5c337088f7cdb26350a07dae6c
Mohtashim Siddiqui <mohtashim@rightsolutions4u.com>
2022-07-14 15:02:34 +0500

    New website

---
+++ RightSolutions4u/RightSolutions4u/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RightSolutions4u.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
