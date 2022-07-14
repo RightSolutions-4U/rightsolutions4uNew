13e8d16a0006fae789dac9ca54731eb715cc9758
Mohtashim Siddiqui <mohtashim@rightsolutions4u.com>
2022-07-15 00:04:21 +0500

    2nd commit

---
+++ RightSolutions4u/RightSolutions4u/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RightSolutions4u.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+        public IActionResult Portfolio()
+        {
+            return View("Portfolio");
+        }
+    }
+}
